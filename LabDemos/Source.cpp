efc375d85a58ef32e1ceb741402662e7eec79bb6
Wallace <Balaniuc>
2021-01-13 10:11:50 -0500

    Completed week 2 demo

--- LabDemos/Source.cpp
+++ LabDemos/Source.cpp
@@ -1,12 +1,20 @@
+// A simple C++ program
 #include <iostream>
-using namespace std;
+using namespace std;	// Use the standard namespace
 
-// This is a comment
+// Main entry point to the application
 int main()
 {
-	int x = 5;
+	double first = 5.25,
+		second = 51.7,
+		third = 20.125,
+		fourth = -10.4;
 
-	cout << x;
+	cout << "Addition: " << first + second << endl;
+	cout << "Subtraction: " << second - third << endl;
+	cout << "Multiplication: " << third * fourth << endl;
+	cout << "Division: " << fourth / first << endl;
+	cout << "Modulus: " << (int)first % (int)second << endl;
 
 	return 0;
 }
